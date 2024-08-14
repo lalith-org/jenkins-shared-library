37dc50eda56c114c169770a201d2f28445836a81
sailalith <lalithvoop@hotmail.com>
2024-08-15 00:16:20 +0530

    shared library updated

--- vars/mainCI.groovy
+++ vars/mainCI.groovy
@@ -1,15 +1,29 @@
 def call () {
     node('ci-server') {
+        stage('CodeCheckout') {
+            sh "find ."
+            sh "find . | sed -e '1d' |xargs rm -rf"
+            if(env.TAG_NAME ==~ ".*") {
+                env.branch_name = "refs/tags/${env.TAG_NAME}"
+            } else {
+                env.branch_name = "${env.BRANCH_NAME}"
+            }
+            checkout scmGit(
+                    branches: [[name: "${branch_name}"]],
+                    userRemoteConfigs: [[url: "https://github.com/lalith-org/expense-${component}"]]
+            )
+        }
         if (env.TAG_NAME ==~ ".*") {
             stage('build application') {
-                print 'npm install'
+                sh 'docker build -t 730335477956.dkr.ecr.us-east-1.amazonaws.com/expense-${component}:${TAG_NAME} .'
             }
             stage('release') {
-                print 'release'
+                sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 730335477956.dkr.ecr.us-east-1.amazonaws.com'
+                sh 'docker push 730335477956.dkr.ecr.us-east-1.amazonaws.com/expense-${component}:${TAG_NAME}'
             }
         } else {
             stage('lint code') {
-                sh 'env'
+                print 'lint code'
             }
             if (env.BRANCH_NAME != "main") {
                 stage('code review') {
